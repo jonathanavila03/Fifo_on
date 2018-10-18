0c5b8066dc0b3d773e65540788d4b6136c9c601f
jonathanavila03 <38079799+jonathanavila03@users.noreply.github.com>
2018-10-18 12:51:17 -0300

    Initial commit

---
+++ app/src/main/java/cl/avarti/fifoon/welco2activity.java
@@ -0,0 +1,13 @@
+package cl.avarti.fifoon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welco2activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welco2activity);
+    }
+}
