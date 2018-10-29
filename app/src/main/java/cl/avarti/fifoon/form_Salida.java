70f264b12ab1d310cbc58d16bb4f7ef224ed2e8b
francoaranguz <francoaranguz@gmail.com>
2018-10-28 22:07:58 -0300

    update form salida
    asd

---
+++ app/src/main/java/cl/avarti/fifoon/form_Salida.java
@@ -0,0 +1,13 @@
+package cl.avarti.fifoon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class form_Salida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form__salida);
+    }
+}
