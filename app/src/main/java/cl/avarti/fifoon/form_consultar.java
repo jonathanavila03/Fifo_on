4668e1bd28d610dd8c6d39e848d209a6d9d1d817
francoaranguz <francoaranguz@gmail.com>
2018-10-28 21:47:00 -0300

    update activity consultar
    update

---
+++ app/src/main/java/cl/avarti/fifoon/form_consultar.java
@@ -0,0 +1,13 @@
+package cl.avarti.fifoon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class form_consultar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_consultar);
+    }
+}
