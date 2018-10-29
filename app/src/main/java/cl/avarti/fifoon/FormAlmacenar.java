81dcce730b781ed04569761a4da38dc66192c9a1
jonathanavila03 <38079799+jonathanavila03@users.noreply.github.com>
2018-10-29 19:26:39 -0300

    mis cambios

--- app/src/main/java/cl/avarti/fifoon/FormAlmacenar.java
+++ app/src/main/java/cl/avarti/fifoon/FormAlmacenar.java
@@ -1,8 +1,7 @@
 package cl.avarti.fifoon;
 
-import android.app.ProgressDialog;
+
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,29 +9,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.app.Activity;
+import com.loopj.android.http.*;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import com.blikoon.qrcodescanner.QrCodeActivity;
+
+import cz.msebera.android.httpclient.Header;
+
 public class FormAlmacenar extends AppCompatActivity {
     private static final int REQUEST_CODE_QR_SCAN = 101;
+    private AsyncHttpClient cliente;
     EditText edit_producto;
     EditText edit_ubicacion;
     EditText edit_fechav;
     Button btn_gua_alma;
     Button btn_gua;
     Button btn_qr;
-    private static final String REGISTER_URL="http://52.67.142.80/jsonfifon/almacenar.php";
+
+    private static final String ALMA_URL="http://52.67.142.80/jsonfifon/almacenar.php?";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_almacenar);
+
         edit_producto = (EditText) findViewById(R.id.txt_producto);
         edit_ubicacion= (EditText) findViewById(R.id.txt_ubicacion);
         edit_fechav = (EditText) findViewById(R.id.txt_fechav);
+        cliente = new AsyncHttpClient();
         btn_gua_alma = (Button) findViewById(R.id.btn_gua_alma);
         btn_gua_alma.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,8 +48,6 @@ public class FormAlmacenar extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 almacenar();
-                Intent intentMenu =new Intent(FormAlmacenar.this,Menuoperador.class);
-                startActivity(intentMenu);
             }
         });
         btn_qr = (Button) findViewById(R.id.qr);
@@ -59,53 +60,37 @@ public class FormAlmacenar extends AppCompatActivity {
         });
     }
 
-    private void almacenar() {
-        String producto = edit_producto.getText().toString().trim().toLowerCase();
-        String ubicacion = edit_ubicacion.getText().toString().trim().toLowerCase();
-        String fechav = edit_fechav.getText().toString().trim().toLowerCase();
-        almacenar_pro(producto, ubicacion, fechav);
-    }
 
-    private void almacenar_pro(String producto, String ubicacion, String fechav){
-        String urlSuffix = "?producto=" + producto + "&ubicacion=" + ubicacion + "&fechav=" + fechav;
-        class AlmacenarEntrada extends AsyncTask<String, Void, String> {
+    void almacenar(){
 
-            ProgressDialog loading;
+        if (edit_producto.getText().toString().isEmpty() || edit_ubicacion.getText().toString().isEmpty() || edit_fechav.getText().toString().isEmpty()){
+            Toast.makeText(FormAlmacenar.this, "Hay Campos Vacios", Toast.LENGTH_SHORT).show();
+        }else {
+            String produ = edit_producto.getText().toString();
+            String ubi = edit_ubicacion.getText().toString();
+            String fechav = edit_fechav.getText().toString();
 
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-                loading = ProgressDialog.show(FormAlmacenar.this, "Espere PorFavor", null, true, true);
-            }
-
-            @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
-                loading.dismiss();
-                Toast.makeText(getApplicationContext(),"Entrada Registrada", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            protected String doInBackground(String... params) {
-                String s = params[0];
-                BufferedReader bufferReader=null;
-                try {
-                    URL url=new URL(REGISTER_URL+s);
-                    HttpURLConnection con=(HttpURLConnection)url.openConnection();
-                    bufferReader=new BufferedReader(new InputStreamReader(con.getInputStream()));
-                    String result;
-                    result=bufferReader.readLine();
-                    return  result;
-
-                }catch (Exception e){
-                    return null;
+            String parametros =  "producto="+produ+"&ubicacion="+ubi+"&fechav="+fechav;
+            cliente.get(ALMA_URL + parametros, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    if (statusCode == 200){
+                        Toast.makeText(FormAlmacenar.this, "Registro Agregado Correctamente", Toast.LENGTH_SHORT).show();
+                        edit_fechav.setText("");
+                        edit_producto.setText("");
+                        edit_ubicacion.setText("");
+                    }
                 }
-            }
 
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                }
+            });
         }
-        AlmacenarEntrada ur=new AlmacenarEntrada();
-        ur.execute(urlSuffix);
-    }
+}
+
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         if (resultCode != Activity.RESULT_OK) {
