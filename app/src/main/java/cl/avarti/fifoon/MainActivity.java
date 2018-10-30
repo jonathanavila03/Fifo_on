ffd8df953c10e1fe5b7931a6787d08289e955251
francoaranguz <francoaranguz@gmail.com>
2018-10-29 23:25:51 -0300

    update login-main
    update

--- app/src/main/java/cl/avarti/fifoon/MainActivity.java
+++ app/src/main/java/cl/avarti/fifoon/MainActivity.java
@@ -1,13 +1,104 @@
 package cl.avarti.fifoon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String LOGIN_URL="http://52.67.142.80/jsonfifon/login.php";
+    public static final String KEY_USER="username";
+    public static final String KEY_PASSWORD="password";
+    public static final String LOGIN_SUCCESS="success";
+    public static final String SHARED_PREF_NAME="tech";
+    public static final String EMAIL_SHARED_PREF="email";
+    public static final String LOGGEDIN_SHARED_PREF="loggedin";
+    private EditText user;
+    private EditText pass;
+    private Button login;
+    private boolean loggedIn=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        user=(EditText)findViewById(R.id.username);
+        pass=(EditText)findViewById(R.id.password);
+        login=(Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        final String username = user.getText().toString().trim();
+        final String password = pass.getText().toString().trim();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, LOGIN_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.trim().equalsIgnoreCase(LOGIN_SUCCESS)){
+
+                            SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                            editor.putBoolean(LOGGEDIN_SHARED_PREF, true);
+                            editor.putString(EMAIL_SHARED_PREF, username);
+
+                            editor.commit();
+
+                            Intent intent = new Intent(MainActivity.this, Menuoperador.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(MainActivity.this, "Invalid username or password", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> prams = new HashMap<>();
+                prams.put(KEY_USER, username);
+                prams.put(KEY_PASSWORD, password);
+
+                return prams;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF_NAME,Context.MODE_PRIVATE);
+        loggedIn = sharedPreferences.getBoolean(LOGGEDIN_SHARED_PREF, false);
+        if(loggedIn){
+            Intent intent = new Intent(MainActivity.this, welco0activity.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/cl/avarti/fifoon/welco0activity.java
@@ -0,0 +1,13 @@
+package cl.avarti.fifoon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class welco0activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welco0activity);
+    }
+}
