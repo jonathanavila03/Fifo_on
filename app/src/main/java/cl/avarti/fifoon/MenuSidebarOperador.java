afea8c80b9377e168e640beaa8cdc5161556a573
francoaranguz <francoaranguz@gmail.com>
2018-11-05 14:12:25 -0300

    update activity sidebar
    asd

---
+++ app/src/main/java/cl/avarti/fifoon/MenuSidebarOperador.java
@@ -0,0 +1,13 @@
+package cl.avarti.fifoon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuSidebarOperador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_sidebar_operador);
+    }
+}
