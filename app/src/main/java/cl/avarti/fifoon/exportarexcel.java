da90f7acc9ed3ffacb37e6b628243c6515592485
francoaranguz <francoaranguz@gmail.com>
2018-11-07 11:52:42 -0300

    update export, splashscreen, calendar
    asd

--- app/src/main/java/cl/avarti/fifoon/exportarexcel.java
+++ app/src/main/java/cl/avarti/fifoon/exportarexcel.java
@@ -1,7 +1,10 @@
 package cl.avarti.fifoon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class exportarexcel extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class exportarexcel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exportarexcel);
+
+
     }
+
+
+
 }
--- app/src/main/java/cl/avarti/fifoon/welco0activity.java
+++ app/src/main/java/cl/avarti/fifoon/welco0activity.java
@@ -1,5 +1,7 @@
 package cl.avarti.fifoon;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class welco0activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welco0activity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(welco0activity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        },1500);
     }
 }
