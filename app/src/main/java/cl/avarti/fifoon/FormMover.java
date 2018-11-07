1dc02638cd5dce936d73d234b9249467bc4a51bf
jonathanavila03 <38079799+jonathanavila03@users.noreply.github.com>
2018-11-07 10:44:31 -0300

    mover listo( sin validaciones)

--- app/src/main/java/cl/avarti/fifoon/FormMover.java
+++ app/src/main/java/cl/avarti/fifoon/FormMover.java
@@ -5,21 +5,17 @@ import android.content.Intent;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import android.widget.TextView;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.blikoon.qrcodescanner.QrCodeActivity;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import java.util.ArrayList;
 import java.util.Locale;
-
 import cz.msebera.android.httpclient.Header;
 
 
@@ -31,12 +27,14 @@ public class FormMover extends AppCompatActivity {
     Button btn_qr_nue;
     Button btn_mic_nue;
     Button btn_mic_ant;
-    private static final String ALMA_URL="http://fifo.esy.es/mover.php?";
-    private AsyncHttpClient cliente;
+    String param;
+    private static final String URL="http://fifo.esy.es/mover.php?";
+    private AsyncHttpClient moverget;
     private static final int REQ_CODE_SPEECH_INPUT=100;
     private static final int REQ_CODE_SPEECH_INPUT2=102;
     private static final int REQUEST_CODE_QR_SCAN = 101;
     private static final int REQUEST_CODE_QR_SCAN2 = 103;
+    String variables;
 
     private TextView datologin;
 
@@ -45,17 +43,16 @@ public class FormMover extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_mover);
 
-
+        moverget = new AsyncHttpClient();
         datologin = (TextView)findViewById(R.id.parametrologinmove);
-
         //TRAER DATO USUARIO LOGIN A ESTE ACTIVITY
-        String param = getIntent().getStringExtra("param");
+        param = getIntent().getStringExtra("param");
         //INSERTAR DATO OBTENIDO EN TEXTVIEW
         datologin.setText("Bienvenido: " + param);
 
 
         ubi_antigua =(EditText) findViewById(R.id.txt_ubi_ant);
-        ubi_nueva = (EditText) findViewById(R.id.txt_ubi_ant);
+        ubi_nueva = (EditText) findViewById(R.id.txt_ubi_nue);
         btn_guardar= (Button) findViewById(R.id.btn_guardar_mover);
         btn_guardar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,7 +68,7 @@ public class FormMover extends AppCompatActivity {
                 startActivityForResult(i, REQUEST_CODE_QR_SCAN);
             }
         });
-        btn_qr_nue = (Button) findViewById(R.id.qr_ant);
+        btn_qr_nue = (Button) findViewById(R.id.qr_nue);
         btn_qr_nue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -95,26 +92,25 @@ public class FormMover extends AppCompatActivity {
         });
     }
 
-    void mover(){
-
-        if (ubi_antigua.getText().toString().isEmpty() || ubi_nueva.getText().toString().isEmpty() ){
+   private void mover(){
+        if (ubi_antigua.getText().toString().isEmpty() || ubi_nueva.getText().toString().isEmpty()|| param.isEmpty()){
             Toast.makeText(FormMover.this, "Hay Campos Vacios", Toast.LENGTH_SHORT).show();
         }else {
             String ubica_ant = ubi_antigua.getText().toString();
             String ubica_nueva = ubi_nueva.getText().toString();
-            String usuario = getIntent().getStringExtra("param");
+            variables =  "ubi_nueva="+ubica_nueva+"&ubi_antigua="+ubica_ant+"&usuario="+param;
 
-            String parametros =  "ubi_nueva="+ubica_nueva+"&ubi_antigua="+ubica_ant+"&usuario="+usuario;
-            cliente.get(ALMA_URL + parametros, new AsyncHttpResponseHandler() {
+           moverget.get(URL + variables, new AsyncHttpResponseHandler() {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                    if (statusCode == 200){
-                        Toast.makeText(FormMover.this, "Registro Agregado Correctamente", Toast.LENGTH_SHORT).show();
-                        ubi_antigua.setText("");
+                    if (statusCode == 200)
+                    {
+                        Toast.makeText(FormMover.this, "El movimiento fue ejecutado correctamente", Toast.LENGTH_SHORT).show();
                         ubi_nueva.setText("");
-
+                        ubi_antigua.setText("");
                     }
                 }
+
                 @Override
                 public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
 
@@ -168,7 +164,7 @@ public class FormMover extends AppCompatActivity {
             if (data != null) {
                 String lectura = data.getStringExtra("com.blikoon.qrcodescanner.got_qr_scan_relult");
                 Toast.makeText(getApplicationContext(), "Leído: " + lectura, Toast.LENGTH_SHORT).show();
-                ubi_antigua.setText(lectura);
+                ubi_nueva.setText(lectura);
 
             }}
         else if (requestCode == REQUEST_CODE_QR_SCAN2 && data == null)
@@ -178,11 +174,11 @@ public class FormMover extends AppCompatActivity {
             return;
         }
 
-        if (requestCode == REQ_CODE_SPEECH_INPUT2){
+        if (requestCode == REQ_CODE_SPEECH_INPUT){
             if (resultCode == RESULT_OK && null != data)
             {
                 ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                ubi_nueva.setText(result.get(0));
+                ubi_antigua.setText(result.get(0));
             }
         }
         if (requestCode == REQ_CODE_SPEECH_INPUT2){
