8c7a54a3ab330cfd5c92bedae3ca7d9bfd5b8cd4
francoaranguz <francoaranguz@gmail.com>
2018-11-07 21:44:24 -0300

    update calendar error
    asd

--- app/src/main/java/cl/avarti/fifoon/FormAlmacenar.java
+++ app/src/main/java/cl/avarti/fifoon/FormAlmacenar.java
@@ -132,11 +132,6 @@ public class FormAlmacenar extends AppCompatActivity {
                         mDateSetListener,
                         day,month,year);
 
-                Calendar c = Calendar.getInstance();
-                c.set(Calendar.HOUR_OF_DAY, 0);
-                c.set(Calendar.MINUTE, 0);
-                c.set(Calendar.SECOND, 0);
-
                 dialog.updateDate(cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH));
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
