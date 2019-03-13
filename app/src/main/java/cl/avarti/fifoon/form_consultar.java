a9df493b0888725a8534273caf388021175b8a86
usuario <usuario@MacBook-Air-de-Jonathan.local>
2019-03-12 22:53:29 -0300

    actualizacion

--- app/src/main/java/cl/avarti/fifoon/form_consultar.java
+++ app/src/main/java/cl/avarti/fifoon/form_consultar.java
@@ -71,7 +71,7 @@ public class form_consultar extends AppCompatActivity {
             Toast.makeText(form_consultar.this, "Hay Campos Vacios", Toast.LENGTH_SHORT).show();
         }else{
             String url = "http://35.226.157.199/JSON/obtenerDatos.php?";
-            String parametros ="producto="+ubicacion.getText().toString();
+            String parametros ="producto="+ubicacion.getText().toString()+"&usr="+datologin;
             cliente.get(url+parametros, new AsyncHttpResponseHandler() {
 
                 @Override
